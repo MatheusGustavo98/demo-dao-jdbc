f7e3f643e1f35bf5ceb742508d171076676c6416
Matheus Godoy <matheusgustavo3103@gmail.com>
2025-02-26 15:22:03 -0300

    DepartmentDao implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -39,17 +39,20 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		
 		System.out.println("\n=== TESTE 5: seller update ===");
 		seller = sellerDao.findById(1);
 		seller.setName("Martha Waine");
 		sellerDao.update(seller);
 		System.out.println("Updated completed!");
 		
+		
 		System.out.println("\n=== TESTE 6: seller delete ===");
 		System.out.print("Digite um ID para teste de exclusão: ");
 		int id = sc.nextInt();
 		sellerDao.deleteById(id);
 		System.out.println("Deleted completed!");
+		
 		
 		sc.close();
 	}
